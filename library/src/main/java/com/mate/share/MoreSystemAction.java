bf7f3ef5d1afa7fcc2174337eca7d453869bbdac
fushenghua <fushenghua2012@126.com>
2018-06-22 10:13:28 +0800

    add share more

--- library/src/main/java/com/mate/share/MoreSystemAction.java
+++ library/src/main/java/com/mate/share/MoreSystemAction.java
@@ -2,14 +2,6 @@ package com.mate.share;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-
-import com.mate.utils.PackageUtils;
-
-import static com.mate.utils.Constants.FACEBOOK_BROWSER_SHARE_URL;
-import static com.mate.utils.Constants.MESSAGER_REQUEST_CODE;
-import static com.mate.utils.Constants.PACKAGE_NAME_MESSAGER;
-import static com.mate.utils.Constants.SHARE_MIME_DATA_TYPE;
 
 /**
  * create by fushenghua
@@ -21,7 +13,7 @@ public class MoreSystemAction implements Action {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/*");
         intent.putExtra(Intent.EXTRA_SUBJECT, text[1]);
-        intent.putExtra(Intent.EXTRA_TEXT, text);
+        intent.putExtra(Intent.EXTRA_TEXT, text[0]);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(Intent.createChooser(intent, text[1]));
     }
